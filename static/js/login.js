39cb01d99bf320569241a76e9e34684e4c492376
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-14 23:42:39 -0600

    refactored, ready for review @mario!

--- static/js/login.js
+++ static/js/login.js
@@ -1,18 +1,9 @@
-var socket = io.connect('http://localhost:3000');
-var buttons;
-var needData;
-var user_info = {"name": "Mike Skirpan"};
+//Get Exhibit JSON
 var numPlayers;
+var redirectionTimer;
+var backendurl = 'quantifiedselfbackend.local';
 var startTimer;
-var players = [];
-
-var player1 = null;
-var player2 = null;
-var player3 = null;
-var player4 = null;
-var url = "http://localhost:7070?"
-//Need to add per-exhibit route
-var backendurl = "quantifiedselfbackend.local"
+var baseurl = "http://localhost:7070?";
 
 $.getJSON("static/data/exhibit.json", function (data) {
     console.log("got the exhibit!");
@@ -20,82 +11,31 @@ $.getJSON("static/data/exhibit.json", function (data) {
     $('#extitle').text(data.title);
     $('#exdetails').text(data.description);
     $('#explay').text(data.players);
-    buttons = data.buttons;
+    redirectionTimer = data.timer;
     numPlayers = data.numPlay
-    needData = data.data;
+    bakendurl = data.backendurl || backendurl;
 }).done(function (data) {
    console.log("all done");
    console.log(data);
 }).fail( function () {
-	console.log("shitty error!");
-});
-
-socket.on('rfid', function (data) {
-    if (players.indexOf(data.user_id) != -1){
-        toastr.error('You are already logged in to this DesignCraft Companion', {positionClass: "toast-top-full-width"});
-
-    }
-    if (players.length == numPlayers) {
-        toastr.error('You are trying to sign in too many users to this companion. Wait your turn!', {positionClass: "toast-top-full-width"});
-        return;
-    }
-
-    clearTimeout(startTimer);
-    console.log(data.user_id);
-    userid = data.user_id;
-    // Will use below AJAX call when ready for live testing..
-
-    // $.ajax({
-    //     type: 'POST',
-    //     url: backendurl,
-    //     data: { "userid" : userid }, 
-    //     success: function(resp) {
-    //         addCard(resp.name);
-    //         players.push(data.user_id);
-    //         startTimer = setTimeout( runGame, 8000);
-    //     },
-    //     error: function(resp) {
-    //         toastr.error('This user is unauthorized to use this companion. Please consider sharing more with DesignCraft for a better experience.', 'User Not Allowed', {positionClass: "toast-top-full-width"});
-    //     }
-    // });
-    addCard(data);
-    players.push(data.user_id);
-    startTimer = setTimeout( runGame, 8000);
-
+    console.log("shitty error!");
 });
 
 function runGame () {
-    for (play in players){
-        url += "userid" + play + "=" + players[play] + "&";
+
+    if (numPlayers == 1) {
+        baseurl += "userid=" + players[play];
+    } else {
+        for (play in players){
+            baseurl += "userid" + play + "=" + players[play] + "&";
+        }
     }
     window.location = url;
 }
 
-
-socket.on('button1', function(){
-    console.log("Button 1 Received!");
-    // socket.emit('pressed');
-
-});
-
-socket.on('button2', function(){
-    console.log("Button 2 Received!");
-    // socket.emit('pressed');
-
-});
-
-socket.on('button3', function(){
-    console.log("Button 3 Received!");
-    // socket.emit('pressed');
-
-});
-
-socket.on('button4', function(){
-    console.log("Button 4 Received!");
-    // socket.emit('pressed');
-
-});
-
+function badPlayer() {
+    toastr.error('This user is unauthorized to use this companion. Please consider sharing more with DesignCraft for a better experience.', 'User Not Allowed', {positionClass: "toast-top-full-width"});
+}
 
 function addCard(userinfo) {
     name = userinfo.user_id;
@@ -112,8 +52,77 @@ function addCard(userinfo) {
     $('#login').append(elem);
 }
 
-function badPlayer() {
-    toastr.error('This user is unauthorized to use this companion. Please consider sharing more with DesignCraft for a better experience.', 'User Not Allowed', {positionClass: "toast-top-full-width"});
+function make_AJAX_call(url, data, tryCount, retryLimit){
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: data,
+        success: function(resp) {
+            name = resp.name || "User";
+            addCard(name);
+            startTimer = setTimeout( runGame, 8000);
+            return true;
+        },
+        error: function(resp) {
+            if (resp.statusCode == 403) {
+                badPlayer();
+            }
+            tryCount++;
+            if (tryCount >= retryLimit){
+                return "bad server";
+            }
+            else { //Try again with exponential backoff.
+                setTimeout(function(){ 
+                    return make_AJAX_call(data, transition, tryCount, retryLimit);
+                }, Math.pow(2, tryCount) * 1000);
+                return false;
+            }
+        }
+    });
 }
 
+function getURLParams() {
+ var re = /[?&]([^&=]+)(?:[&=])([^&=]+)/gim;
+ var m;
+ var v={};
+ while ((m = re.exec(location.search)) != null) {
+   if (m.index === re.lastIndex) {
+     re.lastIndex++;
+   }
+   v[m[1]]=m[2];
+ }
+ return v;
+};
 
+
+var socket = io.connect('http://localhost:3000');
+var players = [];
+
+$(document).ready(function () {
+    //After DOM mounts
+    params = getURLParams();
+    if (params['error']){
+       toastr.error("Something went wrong on DesignCraft's servers. Please try again.", {positionClass: "toast-top-full-width"});
+    }
+    
+    socket.on('rfid', function (data) {
+        if (players.indexOf(data.user_id) != -1){
+            toastr.error('You are already logged in to this DesignCraft Companion', {positionClass: "toast-top-full-width"});
+
+        }
+        if (players.length == numPlayers) {
+            toastr.error('You are trying to sign in too many users to this companion. Wait your turn!', {positionClass: "toast-top-full-width"});
+            return;
+        }
+
+        clearTimeout(startTimer);
+        console.log(data.user_id);
+        userid = data.user_id;
+        permission = make_AJAX_call(bakendurl, {"userid": userid}, 0, 3);
+        addCard(data);
+        players.push(data.user_id);
+
+    });
+
+});
