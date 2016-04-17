f9798b19797b899c0c1ea44c81a508c1d2848f9c
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-17 17:11:45 -0600

    got a special case set up for the romance game

--- gulpfile.js
+++ gulpfile.js
@@ -7,4 +7,11 @@ gulp.task('build', function () {
         .pipe(gulp.dest('static/css'));
 });
 
+gulp.task('build-romance', function () {
+    gulp.src('src/html/romance/index.html')
+        .pipe(gulp.dest('./'));
+    gulp.src('src/css/romance/login.css')
+        .pipe(gulp.dest('static/css'));
+});
+
 gulp.task('default', ['build']);
