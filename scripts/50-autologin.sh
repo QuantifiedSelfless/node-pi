2d36b48302f922e21418dd7d09183083b9e05904
Micha Gorelick <mynameisfiber@gmail.com>
2016-04-23 14:10:46 -0600

    autologin with background setting

---
+++ scripts/50-autologin.sh
@@ -0,0 +1,36 @@
+# ripped out of /usr/bin/raspi-config
+if command -v systemctl > /dev/null && systemctl | grep -q '\-\.mount'; then
+    SYSTEMD=1
+elif [ -f /etc/init.d/cron ] && [ ! -h /etc/init.d/cron ]; then
+    SYSTEMD=0
+fi
+
+if [ $SYSTEMD -eq 1 ]; then
+    systemctl set-default graphical.target
+    ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+else
+    update-rc.d lightdm enable 2
+fi
+sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/"
+
+cp static/img/background.png /usr/share/backgrounds/background.jpg
+cat > /usr/share/gnome-background-properties/debian.xml <<- EOM
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+   <wallpaper>
+    <name>Custom_Back1</name>
+    <filename>/usr/share/backgrounds/background.jpg</filename>
+    <options>center</options>
+    <pcolor>#000000</pcolor>
+    <scolor>#000000</scolor>
+    <shade_type>solid</shade_type>
+   </wallpaper>
+</wallpapers>
+EOM
+
+cat >> /etc/X11/xinit/xinitrc <<- EOM
+xset -dpms
+xset s off
+xset s noblank
+EOM
