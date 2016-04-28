c1a5e8997ee2cc3a787356053c5f842dbdd5ed6f
Micha Gorelick <mynameisfiber@gmail.com>
2016-04-28 12:47:54 -0600

    less interaction necissary

--- setup_scripts/01-network.sh
+++ setup_scripts/01-network.sh
@@ -5,7 +5,7 @@ sed -i 's/127.0.1.1.*/127.0.1.1\t'"$hostname"'/g' /etc/hosts
 echo $hostname > /etc/hostname
 service hostname restart
 
-apt-get install avahi-daemon avahi-dnsconfd avahi-discover avahi-utils libnss-mdns
+apt-get install -y avahi-daemon avahi-dnsconfd avahi-discover avahi-utils libnss-mdns
 insserv avahi-daemon
 insserv avahi-dnsconfd
 service avahi-daemon restart
