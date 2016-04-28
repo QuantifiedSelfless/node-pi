03bd10fd88578ebdf6768fbd88548104a4e982ed
Micha Gorelick <mynameisfiber@gmail.com>
2016-04-28 12:25:57 -0600

    give pi's local dns cache

--- setup_scripts/01-network.sh
+++ setup_scripts/01-network.sh
@@ -4,6 +4,9 @@ echo $hostname > /proc/sys/kernel/hostname
 sed -i 's/127.0.1.1.*/127.0.1.1\t'"$hostname"'/g' /etc/hosts
 echo $hostname > /etc/hostname
 service hostname restart
-apt-get install avahi-daemon
+
+apt-get install avahi-daemon avahi-dnsconfd avahi-discover avahi-utils libnss-mdns
 insserv avahi-daemon
+insserv avahi-dnsconfd
 service avahi-daemon restart
+service avahi-dnsconfd restart
